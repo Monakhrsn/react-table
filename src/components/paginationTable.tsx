08bb17e80becd1a4d0b68e89d4540782c3789386
Monakhrsn <Monakhrsn@gmail.com>
2021-11-16 14:53:27 +0100

    Go to specific page number

--- src/components/paginationTable.tsx
+++ src/components/paginationTable.tsx
@@ -66,6 +66,16 @@ export const PaginationTable = () => {
                 {pageIndex + 1} of {pageOptions.length}
               </strong>
           </span>
+          <span>
+              | Go to page: {' '}
+              <input type="number" defaultValue={pageIndex + 1}
+                onChange={e => {
+                    const pageNumber = e.target.value ? Number(e.target.value) - 1 : 0
+                    gotoPage(pageNumber)
+                }}
+                style={{width: '50px'}}
+              />
+          </span>
           <button onClick={() => gotoPage(0)} disabled= {!nextPage ? !canPreviuosPage : canPreviuosPage}>{'<<'}</button>
         <button onClick={() => previousPage()} disabled={!nextPage ? !canPreviuosPage : canPreviuosPage}>Previuos</button>
         <button onClick={() => nextPage()} disabled={!canNextPage}>Next</button>
