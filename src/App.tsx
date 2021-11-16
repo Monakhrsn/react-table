fe7d21d75fe1004c49e441a22f7699d224ea6165
Monakhrsn <Monakhrsn@gmail.com>
2021-11-16 14:05:24 +0100

    Implement basic pagination

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,15 @@ import React from 'react';
 import './App.css';
 import { BasicTable } from './components/basicTable';
 import FilteringTable from './components/filteringTable';
+import PaginationTable from './components/paginationTable';
 import SortingTable from './components/sortingTable';
 
 
 function App() {
   return (
    <div className='App'>
-     <FilteringTable />
+     <PaginationTable />
+     {/* <FilteringTable /> */}
      {/* <SortingTable /> */}
      {/* <BasicTable /> */}
 
